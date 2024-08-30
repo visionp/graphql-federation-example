19c3efbd81f809662d953f8e232df6afca7e9194
Aleksandr Petrenko <aleksandr.petrenko@evercon.io>
2024-08-30 10:36:17 +0300

    init

--- user-service/src/Domain/UseCase/UserUseCase.php
+++ user-service/src/Domain/UseCase/UserUseCase.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace UserService\Domain\UseCase;
 
+use UserService\Domain\Collection\UserCollection;
+use UserService\Domain\Entity\User;
+use UserService\Domain\Repository\UserRepositoryInterface;
+
 final readonly class UserUseCase
 {
     public function __construct(
         private UserRepositoryInterface $userRepository,
     ) {
+    }
+
+    public function findById(string $id): ?User
+    {
+        return $this->userRepository->findById($id);
+    }
+
+    public function getAllUsers(): UserCollection
+    {
+        return $this->userRepository->findAll();
     }
 }
